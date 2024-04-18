c048ac567d812c7c8ee530f89fffb5732d059b84
Robinpreet1512 <157247263+Robinpreet1512@users.noreply.github.com>
2024-04-18 16:53:21 -0400

    Add files via upload

---
+++ uscard.php
@@ -0,0 +1,451 @@
+<?php include('header.php') ?>
+
+
+
+
+
+<!-- stream_context_get_options -->
+
+<section class='box-h' style=' '>
+
+<section class='menu-box' style=''>
+<div class='menu-section'>
+
+
+<ul class="im">
+<li class="imi"><a href='canada.php'>Canada</a></li>
+<li class="imi"><a href='us.php'>United State</a></li>
+<li class="imi"><a href='uk.php'>United Kingdom</a></li>
+<li class="imi"><a href='au.php'>Australia</a></li>
+<li class="imi"><a href='nz.php'>New Zealand</a></li>
+<li class="imi"><a href='sp.php'>Singapore</a></li>
+<li class="imi"><a href='china.php'>China</a></li>
+
+</ul>
+
+
+
+</div>
+
+</section>
+
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-1/5"  style='color:#D92E7C; font-size:1.4rem;'>USA Green Card</h2>
+    <div class="md:w-4/5 md:pl-6">
+    <!-- <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5" style='color:#D92E7C'>Skilled Workers Visa</h2> -->
+      <p class="leading-relaxed text-base" style='color:black;opacity:1;font-weight:400;'>
+      United States of America or USA is undoubtedly one of the best economies of the world with excellent education system, state-of-the-art business infrastructure, exuberant tourism activities and high standards of living. If you are planning to make the USA your permanent residence, you must first attain a US Green Card.
+
+<br>
+<span style='color:black;font-weight:bolder';>US EB-5 </span><br>
+
+
+
+US EB-5 Program offers an exclusive category of immigration Visa for foreign High Net worth investors. It is one of the fast-track options to get U.S. Green Card and permanent residency through investment. Operated by United States Citizenship and Immigration Services (USCIS), the EB-5 visa program was established by the US Congress in 1990 to facilitate increased investment in the US economy.
+
+<br>
+An Investor with their spouse and unmarried children under the age of 21 can make investment to obtain green card, which allow them the right to permanently live, work and study in the United States.
+
+<br>
+In order to obtain their permanent residency through the EB-5 visa program, foreign investors must meet specific USCIS requirements of Investment which must be a minimum of $1.8 million, or $900,000 in a Targeted Employment Area (high-unemployment or rural areas). As a result of investment applicants are typically required to demonstrate that 10 new jobs are created.
+
+<br>
+      <span style='color:black;font-weight:bolder';>What is a Regional Center?
+
+</span><br>
+Regional Center is an entity, organization or agency that has been approved as such by the USCIS. Regional Centers are eligible to receive immigrant investor capital of US $ 900,000. It focuses on a specific geographic area, also called Targeted Employment Area (high-unemployment or rural areas) within the United Sates. It seeks to promote economic growth to increase export Productivity, creations of new jobs and increased domestic capital investment.
+
+<br>
+      <span style='color:black;font-weight:bolder';>Eligibility Criteria</span><br>
+    <ul style='list-style-type:circle'>
+    <li>Investment: Invest $ 900,000 in a government approved Regional Center
+</li>
+    <li>Source of funds: Source of funds should be legal
+</li>
+    <li>Age: Must be at least 21 years old</li>
+    <li>Experience: No prior business experience required</li>
+    <li>Education: No minimum education requirement</li>
+    <li>Language: English language not necessary</li>
+    <li>Dependent: Dependent under 21 years of age can be included in the application</li>
+   
+    </ul><br>
+
+
+
+
+
+
+
+    </p>
+    </div>
+  </div>
+</section>
+
+
+
+<!-- try  -->
+
+<section class='menu-boxx' style=''>
+<div class='menu-sectionn'>
+
+
+<ul class="im">
+<li class="imii"><a href='canada.php'>Follow Us on Facebook</a></li>
+
+<li class='imii'><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBirla-g-studyvisa-100636402054550&#038;tabs=timeline&#038;width=300px&#038;height=300px&#038;small_header=false&#038;adapt_container_width=true&#038;hide_cover=false&#038;show_facepile=true&#038;appId" >
+</iframe></li>
+
+
+<li><iframe  src="https://www.youtube.com/embed/SUQmP3spU0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+
+</ul>
+
+
+
+</div>
+
+</section>
+
+<!-- try exit  -->
+
+</section>
+
+
+
+
+
+<!-- flagg   ¿ -->
+
+
+<!-- tehhorizontal class  -->
+
+<section class="hc">
+
+
+
+
+<!-- $  -->
+
+<!-- 
+<section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-gray-100">
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+         <span class="font-semibold title-font text-gray-700" style='color:#D92E7C; font-weight:400;font-size:1rem'>Tier 4 - Student (Adult) Visa</span>
+        </div>
+        <div class="md:flex-grow">
+          
+          <p class="leading-relaxed"  style='font-size:.8rem'>
+          The student visa under this category is for students who intend to participate in a full time course at a UK institution of any post-16 education program..
+          
+          
+                                       </p>
+          </a>
+        </div>
+      </div>
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <span class="font-semibold title-font text-gray-700"  style='color:#D92E7C; font-weight:400;font-size:1rem'>Universities</span>
+        </div>
+        <div class="md:flex-grow">
+          <p class="leading-relaxed" style='font-size:.8rem'>
+          Middlesex University	The Burroughs, London
+2	Chester University	Parkgate Rd, Chester
+3	Bradfordshire University	Vicarage St, Luton
+4	London School of Business and Finance (LSBF)	London
+5	Birmingham City University	Birmingham
+6	Greenwich University	London
+7	De Montfort University	Gateway House, Leicester
+8	Oxford Brookes University	Oxford
+
+                    </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section> -->
+
+
+
+
+
+
+<!-- try  -->
+
+<!-- $ 
+
+<section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-gray-100">
+    
+    
+
+       <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+       <span class="font-semibold title-font text-gray-700"  style='color:#D92E7C; font-weight:400;font-size:1rem'>Tier 4 - Student (Child) Visa</span>
+        </div>
+        <div class="md:flex-grow">
+          <p class="leading-relaxed" style='font-size:.8rem'>
+          The Tier 4 – Student (Child) visa is applicable for students, between the ages of 4 and 17 years, intending to study at a UK educational institution..
+          
+                              </p>
+         
+        </div>
+      </div>
+       <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+       <span class="font-semibold title-font text-gray-700"  style='color:#D92E7C; font-weight:400;font-size:1rem'>Course</span>
+        </div>
+        <div class="md:flex-grow">
+          <p class="leading-relaxed" style='font-size:.8rem'>
+          A UK undergraduate education gives you an amazing opportunity to immerse yourself in a subject that fascinates you..
+                                        </p>
+         
+        </div>
+      </div>
+    
+    </div>
+  </div>
+</section> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- try  -->
+
+<section class="faq-section">
+<div class="container">
+  <div class="row">
+                    <!-- ***** FAQ Start ***** -->
+                    <div class="col-md-6 offset-md-3">
+
+                        <div class="faq-title text-center pb-3">
+                            <h2>FAQ</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6 offset-md-3">
+                        <div class="faq" id="accordion">
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-1">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-1" data-aria-expanded="true" data-aria-controls="faqCollapse-1">
+                                            <span class="badge">1</span>What is Canada immigration Visa ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-1" class="collapse" aria-labelledby="faqHeading-1" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>It is a document that permits a person to work and live in one of the Canadian province or territory. It comes with several responsibilities and may be revoked in case the holder does not obey the Canadian residency obligations or is found guilty of conducting criminal activities in the country. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-2">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-2" data-aria-expanded="false" data-aria-controls="faqCollapse-2">
+                                            <span class="badge">2</span> When can i obtain citizenship of Canada ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-2" class="collapse" aria-labelledby="faqHeading-2" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>The Canadian Citizenship can typically be obtained after a period of three years of Permanent residence in Canada.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-3">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-3" data-aria-expanded="false" data-aria-controls="faqCollapse-3">
+                                            <span class="badge">3</span>Why is Canadian Permanent Residential Card ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-3" class="collapse" aria-labelledby="faqHeading-3" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>A Canadian Permanent Resident Card is a small, secured plastic card which contains the cardholder’s personal details and confirms the holder’s status as a Permanent Resident of Canada.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-4">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-4" data-aria-expanded="false" data-aria-controls="faqCollapse-4">
+                                            <span class="badge">4</span> What is the procedure of getting an Australian Visa renewed ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-4" class="collapse" aria-labelledby="faqHeading-4" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>The renewal process is pretty simple. All you need to do is connect to the nearest Australian Visa office and appoint an interview. Furnish all the desired documents to support your situation and pay tax at the time of new visa allotment.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-5">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-5" data-aria-expanded="false" data-aria-controls="faqCollapse-5">
+                                            <span class="badge">5</span> What is Point Check ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-5" class="collapse" aria-labelledby="faqHeading-5" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p> A point check is a transparent and objective method of selecting migrants with skills and attributes needed in Australia. The Ministry of Immigration and Citizenship has scaled the point check of minimum 60. The higher the points, the better chances you have of getting through the immigration process.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-6">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-6" data-aria-expanded="false" data-aria-controls="faqCollapse-6">
+                                            <span class="badge">6</span> Do i have to pass any evaluation ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-6" class="collapse" aria-labelledby="faqHeading-6" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>In order to apply for a visa you will have to get a skill assessment done by furnishing certain diplomas/documents honored by the Australian Immigration Department and pass the English proficiency test – IELTS. You will also have to undergo police verification and medical tests, which are a part of the Australian Immigration process.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="faqHeading-7">
+                                    <div class="mb-0">
+                                        <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-7" data-aria-expanded="false" data-aria-controls="faqCollapse-7">
+                                            <span class="badge">7</span> Why BirlaG ?
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div id="faqCollapse-7" class="collapse" aria-labelledby="faqHeading-7" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <p>BIRLA G is one of the leading global resettlement solution service providers offering full range of services in the area of SKILLED, STUDENT AND BUSINESS IMMIGRATION.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer  -->
+
+
+
+<section class="eight-section">
+
+
+<div  class="footerimage jumbotron">
+    <div class="f1 footer footerfont"><h1>Birla - G</h1></div>
+  
+  <div class="row mt-5 ">
+  
+    <div class="col-4">
+      <ul class="footerfont" style="list-style-type:none;">
+        <li><i class="far fa-envelope"></i> info@birlag.com</li><br>
+        <li><i class="fas fa-phone-square-alt"></i> +91 9041441465</li><br>
+        <li><i class="fas fa-phone-square-alt"></i> KB Tower, Plot No. F-465,Nr.Ajit News Paper, Phase 8B, SAS Nagar, Mohali</li>
+      </ul>
+    </div>
+  
+  
+  
+  <div class="col-4 ">
+  <ul class=""  style="list-style-type:none;">
+  <a href='index.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> HOME</li></a><br>
+    <!-- <a href='index.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> OUR PRODUCTS</li><br> -->
+    <a href='services.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> OUR SERVICES</li></a><br>
+    <a href='contact.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> ABOUT US</li></a>
+  </ul>
+  </div>
+  
+  
+  
+  
+  <div class="col-4 ">
+    <ul  style="list-style-type:none;">
+      <li><h3 class='stay-conn'>STAY CONNECTED</h3></li><br>
+      <li><img src="images/instaicon.jpg" class="lu" alt="">         Instagram</li><br>
+      <li><a href='https://www.facebook.com/officialbirlagvisa/'><img src="images/fbicon.png" class="lu" alt=""> Facebook</a></li><br>
+      <li><a href='https://twitter.com/intent/tweet?url=http%3A%2F%2Fbirlagvisa.com%2F&text='><img src="images/twicon.png" class="lu" alt=""> Twiter</a></li>
+    </ul>
+  
+  
+  
+  </div>
+  
+  </div>
+  
+  
+  
+  </div>
+  </section>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>	
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/jquery.easing.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>	
+    <script src="js/menu.js"></script>
+    <script src="js/materialize.js"></script>	
+    <script src="js/jquery.scrollto.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>	
+    <script src="js/hero-request-form.js"></script>
+    <script src="js/hero-register-form.js"></script>
+    <script src="js/request-form.js"></script>	
+    <script src="js/contact-form.js"></script>
+    <script src="js/comment-form.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>	
+    <script src="js/jquery.validate.min.js"></script>	
