d070818c64981d039d3f0d3e25320e5d51506050
Quawi Mudassir <quawimudassir@gmail.com>
2024-04-18 16:36:47 -0400

    Added Contacts

---
+++ contact.php
@@ -0,0 +1,274 @@
+<?php include('header.php') ?>
+
+
+<section class="contact-one">
+
+
+<div class="posss">
+<h3 class='contact-help'>Need Any Help We'Re Always Here</h3>
+<h2 class="just-click">Just click on the call button</h2>
+<div class='just-call'>
+<a href="tel:+91 9041441465"  class=""><i class="fas fa-phone"></i> <span style='color:hotpink'> +91 9041441465</span></a>
+</div>
+
+
+
+</div>
+</section>
+
+
+
+<section class="address">
+
+<div class="image-birla">
+<img src="./img/pad.png" class='birla-img-house' alt="">
+</div>
+
+<div class="ad">
+
+<h3>Birla</h3>
+<h5 class='add'>K & B Tower, Plot No. F-465, Industrial Area, Nr. Ajit News Paper, Phase 8B, SAS Nagar, Mohali</h5>
+<h5  class='add'>Birlagstudyvisa@gmail.com</h5>
+<h5  class='add'>+91 8544964581 , 
++91 9041441465</h5>
+<h5  class='add'>0172- 5030032</h5>
+</div>
+
+
+
+</section>
+
+
+
+<!-- tail  -->
+
+
+
+
+
+<!-- <section class="text-gray-600 body-font relative ">
+  <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+    <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+
+      <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109782.86712323576!2d76.62733957899269!3d30.69830520714667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef36fd8df87d%3A0x77d1d49582c1528b!2sK%26B%20Tower!5e0!3m2!1sen!2sin!4v1616344650725!5m2!1sen!2sin"  style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
+      <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+        <div class="lg:w-1/2 px-6">
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+          <p class="mt-1">K & B Tower, Plot No. F-465, Industrial Area, Nr. Ajit News Paper, Phase 8B, SAS Nagar, Mohali</p>
+        </div>
+        <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+          <a class="text-pink-500 leading-relaxed">Birlagstudyvisa@gmail.com</a>
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+          <p class="leading-relaxed">+91 8544964581</p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section> -->
+
+
+<section class="text-gray-600 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109782.86712323576!2d76.62733957899269!3d30.69830520714667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef36fd8df87d%3A0x77d1d49582c1528b!2sK%26B%20Tower!5e0!3m2!1sen!2sin!4v1616344650725!5m2!1sen!2sin" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <!-- <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md"> -->
+      <!-- <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p> -->
+
+      <!-- <div id='fo' id="request-1" class="bg-image wide-60 request-form-section division" > -->
+				<!-- <div class="container"> -->
+					<!-- <div class="row d-flex align-items-center"> -->
+
+
+						<!-- REQUEST FORM TEXT -->
+						
+
+
+						<!-- REQUEST FORM -->
+						<!-- <div class="col-md-6 col-xl-5 offset-xl-1" > -->
+							<div  class="text-center mb-60" >
+								<form class="row  bg-lightgrey mr-10 ml-10" method='post' action='formm.php' style='height:auto; width:auto; padding:30px; border-radius:3%;'>
+									<!-- Request Form Text -->      
+                                    <div class="col-md-12">
+                                        <h5 class="h5-lg">Free Consultation</h5>	
+                                    </div> 
+
+                                    <!-- Request Form Input -->
+					                <div id="input-name" class="col-md-12">
+					                	<input type="text" name="name" class="form-control mb-2 phone" placeholder="Enter Your Name*" required> 
+					                </div>
+					                     
+					                <!-- Request Form Input -->       
+					                <div id="input-email" class="col-md-12">
+					                	<input type="text" name="email" class="form-control mb-2 email" placeholder="Enter Your Email*" required> 
+					                </div>
+
+									<!-- Request Form Input -->
+					                <div id="input-phone" class="col-md-12">
+					                	<input type="tel" name="phone" class="form-control mb-2 phone" placeholder="Enter Your Phone Number*" required> 
+					                </div>	
+
+					                <!-- Request Form Select -->
+					                <div id="input-visa" class="col-md-12 input-visa">
+					                    <select id="inlineFormCustomSelect1" name="visa" class="custom-select mb-2 visa" required>
+					                        <option value="">Select Visa</option>  
+					                      	<option>Student Visa</option>
+					                      	<option>Travel visa</option>
+					                      	<option>Working Visa</option>
+					                      	<option>Business Visa</option>
+					                      	<option>Visitor Visa</option>
+					                      	<option>PR Visa</option>
+					                    </select>
+					                </div>
+
+					                <!-- Request Form Select -->
+					                <div id="input-country" class="col-md-12 input-country">
+					                    <select id="inlineFormCustomSelect2" name="country" class="custom-select mb-2 country" required>
+					                        <option value="">Visa For</option>  
+					                      	<option>Australia</option>
+					                      	<option>Canada</option>
+					                      	<option>United Kingdom</option>
+					                      	<option>USA</option>
+					                      	<option>Singapore</option>
+					                      	<option>Netherlands</option>
+					                    </select>
+					                </div>
+						                                            
+					                <!-- Request Form Button -->
+					                <div class="col-md-12 form-btn">  
+                                    <input type="submit" value="Submit" name="submit"  class="btn btn-primary ">
+					                </div>
+					                                                              
+					                <!-- Request Form Message -->
+					                <!-- <div class="col-md-12 request-form-msg text-center">
+					                	<div class="sending-msg"><span class="loading"></span></div>
+					                </div>   -->
+					                                              
+					            </form>
+							</div>
+						<!-- </div>	END REQUEST FORM -->
+
+
+					</div>	<!-- End row -->
+				</div>  <!-- End container -->
+			<!-- </div>	END REQUEST FORM -->
+    <!-- </div> -->
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- try end  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="eight-section">
+
+
+<div  class="footerimage jumbotron">
+    <div class="f1 footer footerfont"><h1>Birla - G</h1></div>
+  
+  <div class="row mt-5 ">
+  
+    <div class="col-4">
+      <ul class="footerfont" style="list-style-type:none;">
+        <li><i class="far fa-envelope"></i> info@birlagvisa.com</li><br>
+        <li><i class="fas fa-phone-square-alt"></i>+91 90414-41465</li><br>
+        <li><i class="fas fa-phone-square-alt"></i> KB Tower, Plot No. F-465,Nr.Ajit News Paper, Phase 8B, SAS Nagar, Mohali</li>
+      </ul>
+    </div>
+  
+  
+  
+  <div class="col-4 ">
+  <ul class=""  style="list-style-type:none;">
+  <a href='index.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> HOME</li></a><br>
+    <!-- <a href='index.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> OUR PRODUCTS</li><br> -->
+    <a href='services.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> OUR SERVICES</li></a><br>
+    <a href='contact.php'><li class="footerhover"><i class="fas fa-chevron-right"></i> ABOUT US</li></a>
+  </ul>
+  </div>
+  
+  
+  
+  
+  <div class="col-4 ">
+    <ul  style="list-style-type:none;">
+      <li><h3 class='stay-conn'>STAY CONNECTED</h3></li><br>
+      <li><img src="images/instaicon.jpg" class="lu" alt="">         Instagram</li><br>
+      <li><img src="images/fbicon.png" class="lu" alt=""> Facebook</li><br>
+      <li><img src="images/twicon.png" class="lu" alt=""> Twiter</li>
+    </ul>
+  
+  
+  
+  </div>
+  
+  </div>
+  
+  
+  
+  </div>
+  </section>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>	
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/jquery.easing.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>	
+    <script src="js/menu.js"></script>
+    <script src="js/materialize.js"></script>	
+    <script src="js/jquery.scrollto.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>	
+    <script src="js/hero-request-form.js"></script>
+    <script src="js/hero-register-form.js"></script>
+    <script src="js/request-form.js"></script>	
+    <script src="js/contact-form.js"></script>
+    <script src="js/comment-form.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>	
+    <script src="js/jquery.validate.min.js"></script>	
