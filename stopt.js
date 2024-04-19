b9116297de4e546ab246d8b33565664a07f814d0
Robinpreet1512 <157247263+Robinpreet1512@users.noreply.github.com>
2024-04-19 13:10:34 -0400

    Add files via upload

---
+++ stopt.js
@@ -0,0 +1,21 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+        var x = screen.width;
+    
+        
+        
+        if (x< 993){
+            document.getElementById('deo1').removeAttribute("data-aos");
+           
+
+        }
+    // comsole.log(x)
+
+
+ }, false);
+
+
+
