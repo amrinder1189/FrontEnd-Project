80f174e3e6d3347af0268a5a94b386cc35350153
amrinder1189 <samrinder1189@gmail.com>
2024-04-19 23:47:25 -0400

    title updated in header.php

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Birla-G Stud Visa</title>
+    <title>Birla-G Project by Creators</title>
 
     <link rel="shortcut icon" href="./Birla G Logo.png" type="image/x-icon">
     <link rel="stylesheet" href="main.css">
