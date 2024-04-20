25f99b16504ed128b9715f1f2360ae6ccc004e83
amrinder1189 <samrinder1189@gmail.com>
2024-04-19 23:52:25 -0400

    css files comments done

--- js.js
+++ js.js
@@ -1,40 +1,28 @@
+// Added js for animation
 AOS.init({
-    delay: 100,
-    duration: 800, 
-    easing: 'ease',
-    once: true,
-    mirror:false
+  delay: 100,
+  duration: 800,
+  easing: "ease",
+  once: true,
+  mirror: false,
 });
 
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var x = screen.width;
 
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-
-        var x = screen.width;
-    
-        
-        
-        if (x< 993){
-            document.getElementById('demoo').removeAttribute("data-aos");
-            document.getElementById('demo1').removeAttribute("data-aos");
-            document.getElementById('demo2').removeAttribute("data-aos");
-            document.getElementById('demo3').removeAttribute("data-aos");
-            document.getElementById('demo4').removeAttribute("data-aos");
-            document.getElementById('demo5').removeAttribute("data-aos");
-            document.getElementById('demo6').removeAttribute("data-aos");
-            document.getElementById('demo7').removeAttribute("data-aos");
-
-
-        }
+    if (x < 993) {
+      document.getElementById("demoo").removeAttribute("data-aos");
+      document.getElementById("demo1").removeAttribute("data-aos");
+      document.getElementById("demo2").removeAttribute("data-aos");
+      document.getElementById("demo3").removeAttribute("data-aos");
+      document.getElementById("demo4").removeAttribute("data-aos");
+      document.getElementById("demo5").removeAttribute("data-aos");
+      document.getElementById("demo6").removeAttribute("data-aos");
+      document.getElementById("demo7").removeAttribute("data-aos");
+    }
     // comsole.log(x)
-
-
- }, false);
-
-
-
+  },
+  false
+);
